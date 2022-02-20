b4c8d95839b6daabfbb07a62cda82232e9d3cc0a
ekhalturin-ki15 <1tigrin@mail.ru>
2022-02-20 14:31:36 +0700

    Добавьте файлы проекта.

---
+++ Курс_от_20_02_С++_начало.cpp
@@ -0,0 +1,36 @@
+﻿
+
+
+// GitHub
+int main()   // start
+{
+	int I; I = 1443434; // 4 байт
+	long L; // 4 байт
+
+	long long LL; LL = 1349820948329048; // 8 байт
+	long long int LLI = 14342; //  8 байт
+
+	__int32 I32; 
+	__int64 LL64;  // 8 байт
+
+	char C;  // 1 байт
+	C = 'a'; // '' один символ
+
+	//string
+	float F; F = 1.443434; // 4 байт
+	double D; D = 1.325472348957894375; // 8 байт
+
+	bool B;
+	B = 1;  B = true; 
+	B = 0;  B = false;
+
+	// Unreal Engine (C++)
+	int AmountBrick;
+	bool bAiming;
+
+	//int InAmount;
+	//int OutAmount;
+
+	//int amountBrick;
+	//int Amount_Brick;
+}
