f97ed1aad17fcd6e1b523d8af15f124abede0573
ekhalturin-ki15 <1tigrin@mail.ru>
2022-02-20 15:12:26 +0700

    1 запись

--- Курс_от_20_02_С++_начало.cpp
+++ Курс_от_20_02_С++_начало.cpp
@@ -3,18 +3,26 @@
 
 // GitHub
 int main()   // start
-{
+{ // Локальная область
 	int I; I = 1443434; // 4 байт
 	long L; // 4 байт
 
-	long long LL; LL = 1349820948329048; // 8 байт
+	long long LL; LL = -1349820948329048; // 8 байт
 	long long int LLI = 14342; //  8 байт
 
-	__int32 I32; 
+	__int32 I32 = 0; 
 	__int64 LL64;  // 8 байт
 
-	char C;  // 1 байт
+	char C;  // 1 байт ascii 256 
 	C = 'a'; // '' один символ
+	C = 97;
+	C = 32;
+	C = '0'; 
+
+	//char32_t C32; // 4 байт
+
+
+	// f 10 выполнения по шагам
 
 	//string
 	float F; F = 1.443434; // 4 байт
@@ -23,9 +31,56 @@ int main()   // start
 	bool B;
 	B = 1;  B = true; 
 	B = 0;  B = false;
+	I32 + 100ll;
+
+	100; // int 4 байт
+
+	100ll; // long long
+	1.f;// float
+	1. ;// double 8 байт
+	
+
 
 	// Unreal Engine (C++)
-	int AmountBrick;
+	int AmountBrick = I32;
+
+	// Арифметические операции
+	//  +   -    *     /               % 
+
+	AmountBrick = 5;
+	F = 10;
+	D = 52;
+	I32 = 92192123434;
+	LL64 = -2343252345;
+
+	I32 = (AmountBrick + F) * (D)   ;
+	(AmountBrick * F) / D;// <-
+
+	LL64 = I32 = 0;
+
+	LL64 =  10 + ( I32 = (AmountBrick + F) * (D) ); // = 15  (+ 6 )
+	// float  /  (вещественное)
+
+
+
+	LL64 = I32 / 100;
+	D = I32 / 100;
+
+
+	LL64 = F / 100;
+	D = F / 100;
+
+
+	LL64 = I32 / 100.;
+	D = I32 / 100.;
+
+
+
+
+	// c++   float   Делитель или частное  -> вещественные
+	// c++  оба целочисл   -> целочисленный
+
+	// f9 точка остановки f5 запуск с отладкой
 	bool bAiming;
 
 	//int InAmount;
@@ -33,4 +88,6 @@ int main()   // start
 
 	//int amountBrick;
 	//int Amount_Brick;
+
+
 }
