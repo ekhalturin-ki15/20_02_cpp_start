eb64e67824d8ee8b5e606516c2aec9c1216b9e7d
ekhalturin-ki15 <1tigrin@mail.ru>
2022-02-27 16:55:29 +0700

    Циклы

--- Занятие от 27.02.cpp
+++ Занятие от 27.02.cpp
@@ -21,133 +21,59 @@ int main()
 	++a;
 #endif
 
-
-	int a = 4343;
+	int a;
 	int b;
 
-	int& n = b;
-	//cin >> a >> b;
-	int max = 3294;
-
-	srand(time(0));
-
-	int random = 5 + (rand() % 11);
+	//a++, b++;
 
 
-	if ( cout<<"3" )
-	{
-		cout << a << " < " << n;
-
-	}
-	else
-	{
-lable:
-		cout << a << " >= " << n;//else
-	}
-
-	cout << "\n";
-
-	//a + "dfdf " +// to_string(b);
-	//to_string()
-
-
-
-	cout << ((a > b) ? "34" :  "dfdfdf"); // Типы совместимы void (ничего не возвращая - нельзя использовать)
-	cout << max;
-	max--;
-
-	if (max != 0)
-		goto lable;
-
-
-
-
-	//cout << string((a < n) ? string(" < ") : string(" >= "));
-
-
-
-	const int stop_program = 10;
-	//cin >> q;
 	cin >> a;
 
-
-	//char ch = getchar(); // Считать один символ
-	//ch = getchar();
-	//const bool true = 1;
-
-	//goto (__dgfhdsgjkh);
+	//Вначе инициализ до первой ; (только один раз)
 
 
+	//Проверяем условие
+	// Выполняем то, что в {}
+	// Выполняем код после второй ;
+	//Проверяем условие
+	//..
+	
+
+	for (int i = 0; i < 10000; ++i) // Счетчик и он участвует в условии (пробежка по диапазону)
+		cout << "\\/";
 
 
-
-	if (a == 1)
-		goto _default;
-	else
-		if (a == 0)
-			goto _case0;
-		else
-			goto _case1;
-
-	//switch
-		_default:
-			{}
-		_case1: // Старт, откуда продолж программа
-			a = 1000;
-			//break; // Иначе перейдём дальше
-		_case0:
-			a = 2000;
-			//return 0;
-
-	switch (a)
+	for (int j = 0; (j < a); ++j, "dsf")
 	{
-	default:
-	{}
-	case 1: // Старт, откуда продолж программа
-		a = 1000;
-		//break; // Иначе перейдём дальше
-	case 0:
-		a = 2000;
-		break;
-		//return 0;
-		switch (b)
-		{
-		case 1: 
-			a = 1000;
-			break; // Выйти из текущего switch
-		}
-
+		int q = 5;
+		cout << q << " " << j << "\n";
+		q = 244234;
+		continue; //Перейти к следующему шагу цикла
+		break; // выйти
 	}
-	//printf;
-	//scanf
-	int i;
-	float f;
-	char ch;
-	int i2; // не иниц переменная
 
-	i = i2;
-
-	int* ptr = new int;
-	//scanf_s безопасный вариант разрешенный microsoft на него не ругается SDL
-	scanf_s("%d %f %c\n%d", ptr, &f, &ch, &i2); // cin ввод игнор спец символов Записывае по адресу
-
-	printf_s("%d %f %c\n%d", i, f, ch, i2); // cout вывод Важно как выводим
+	//Тоже самое
+	int j = 0;
+	while (j < a) // true - выполняется
+	{
+		int q = 5;
+		cout << q << " " << j << "\n";
+		q = 244234;
+		++j, "dsf";
+	}
 
 
+	do //Обязательно 1 раз выполняется
+	{
+		int q = 5;
+		cout << q << " " << j << "\n";
+		q = 244234;
+		++j, "dsf";
+	} while (j < a); // True - выпонение не забудьте ;
 
 
 
 
-#ifdef _DEBUG
-	cin >> a;
 
-#else
-	cin >> b;
 
-#endif
-
-	return 0; // Код ошибки 0 - Всё отлично
-
-	//return 1; //     /0
-	//return 2; //      
 }
