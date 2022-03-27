13835bf9c5f2b0190c9340d09b29e24a7dd388a9
ekhalturin-ki15 <1tigrin@mail.ru>
2022-03-27 14:22:04 +0700

    2

--- Занятие от 27.02.cpp
+++ Занятие от 27.02.cpp
@@ -7,23 +7,67 @@
 using namespace std;
 //getline(cin, s);
 
+//#define SUM(a, b) a + b
+
+
+
+template <typename T, typename P>
+auto sum(T a, P b)
+{
+	if (a < b)
+	{
+		return a + b; // float
+	}
+	else
+	{
+		return 0.f; // float
+	}
+}
+
+
+
+template <typename T, typename P>
+auto sum2(T a, P b)
+{
+	return string(a) + string(b);
+
+}
+
+
+//auto sum2(auto a, auto b)
+//{
+//	return a + b;
+//}
+
+
+
 struct Str
 {
 	string s;
+
 
 	void solve()
 	{
 		getline(cin, s);
 
+		//cout <<  sum2(1, 5) << "\n";
+		cout << sum2(1.6, 5.6) << "\n";
+		cout << sum2(1, 5.6) << "\n";
+		cout << sum2(1, 5) << "\n";
 
+		//cout << sum(1, 4) << "\n";
+		//cout << sum(1, 4.5f) << "\n"; // ошибка
+		//cout << sum(1.5f, 4) << "\n"; // ошибка
+		//cout << sum(1.5f, 4.f) << "\n"; // 
 
+		//cout << sum(s, s) << "\n";
 
+		//cout << SUM(s, "3443");
+		//s + "3443"
+
+		cout << "\n";
 	}
-
-
-
 };
-
 
 
 
@@ -41,7 +85,18 @@ int main()
 	cin >> test; cin.ignore();
 #endif
 
+
+
 	vector<Str> str;
+
+	//()
+	//[]
+	// >>
+	// 
+	//*(a + 5)
+	// <>
+
+
 	for (int number_test = 0; number_test < test; number_test++)
 	{
 		str.clear();
