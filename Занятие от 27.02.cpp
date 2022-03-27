92bc3cc572b93b5f551b1f6a392e4919d7cb55ea
ekhalturin-ki15 <1tigrin@mail.ru>
2022-03-27 15:00:39 +0700

    Псевдонимы

--- Занятие от 27.02.cpp
+++ Занятие от 27.02.cpp
@@ -5,67 +5,67 @@
 #include <queue>
 
 using namespace std;
-//getline(cin, s);
 
 //#define SUM(a, b) a + b
 
-
-
-template <typename T, typename P>
-auto sum(T a, P b)
+enum Stage
 {
-	if (a < b)
-	{
-		return a + b; // float
-	}
-	else
-	{
-		return 0.f; // float
-	}
-}
+	start = 5,
+	process = 2,
+	stop = 19
+};
 
-
-
-template <typename T, typename P>
-auto sum2(T a, P b)
+// Возвращать псевдо для функций имеет смысл, если арг псевдоним, и мы возвр его
+int& sum(int& a)
 {
-	return string(a) + string(b);
-
+	int t;
+	return a;
 }
-
-
-//auto sum2(auto a, auto b)
-//{
-//	return a + b;
-//}
-
 
 
 struct Str
 {
 	string s;
 
-
-	void solve()
+	string& solve()
 	{
 		getline(cin, s);
 
-		//cout <<  sum2(1, 5) << "\n";
-		cout << sum2(1.6, 5.6) << "\n";
-		cout << sum2(1, 5.6) << "\n";
-		cout << sum2(1, 5) << "\n";
+		int n;
+		cin >> n;
 
-		//cout << sum(1, 4) << "\n";
-		//cout << sum(1, 4.5f) << "\n"; // ошибка
-		//cout << sum(1.5f, 4) << "\n"; // ошибка
-		//cout << sum(1.5f, 4.f) << "\n"; // 
+		Stage stage = Stage(n);
 
-		//cout << sum(s, s) << "\n";
+		switch (stage)
+		{
+		case start:
+			//cout << "starting\n";
+			break;
+		case process:
+			//cout << "process\n";
+			break;
+		case stop:
+			//cout << "ending\n";
+			break;
+		default:
+			//cout << "no stage\n";
+			break;
+		}
+		//double& d = reinterpret_cast<double>(n);
 
-		//cout << SUM(s, "3443");
-		//s + "3443"
+		// void* t = &n;
+		// double& d = ((double*)(t*));
 
-		cout << "\n";
+		int& qq = n;
+		sum(n);
+		int& q = sum(n);
+		q = 100;
+		cout << q << "\n";
+		
+		cout << sum(n) << "\n";
+
+
+		return s;
 	}
 };
 
@@ -85,16 +85,7 @@ int main()
 	cin >> test; cin.ignore();
 #endif
 
-
-
 	vector<Str> str;
-
-	//()
-	//[]
-	// >>
-	// 
-	//*(a + 5)
-	// <>
 
 
 	for (int number_test = 0; number_test < test; number_test++)
@@ -102,6 +93,9 @@ int main()
 		str.clear();
 		str.resize(1);
 		str.front().solve();
+#ifdef _DEBUG
+		cout << "\n" << number_test << " ------------ \n";
+#endif
 	}
 
 }
