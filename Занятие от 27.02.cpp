c1332c320f39fa76478c682d1d2cf279592fb0f2
ekhalturin-ki15 <1tigrin@mail.ru>
2022-04-03 17:02:57 +0700

    Исключения

--- Занятие от 27.02.cpp
+++ Занятие от 27.02.cpp
@@ -8,7 +8,7 @@
 #include <set>
 #include <random>
 #include <map>
-
+#include <exception>
 
 using namespace std;
 
@@ -23,8 +23,6 @@ struct cmp
 	}
 	// 9 == 11
 	// == !(l<r) && !(r<l)
-
-
 	// abs(9 - 10) < abs(11 - 10) false
 
 };
@@ -52,141 +50,22 @@ int main()
 	cin >> a >> b;
 	try
 	{
-		a / b;
-		cout << "Все хорошо";
+		if (b == 0) throw logic_error("Деление на ноль");
+		if (a > 100) throw overflow_error("Большое число");
+		cout <<  a / b;
 	}
-	catch (...)
+	catch (std::logic_error exc)
 	{
-		cout << "Ошибка";
+		cout << exc.what();
 	}
+	catch (std::overflow_error exc)
+	{
+		cout << "INF";
+	}
+	catch (...)// default
+	{
 
+	}
 	
-	cout << "наша программа работает" << a;
-
-
-
-
-	return 0;
-	cin >> n;
-
-	vector<int> v = { 6, 5, 7, 4, 8, 3, 9, 2, 10, 1, 11 };
-	vector<int> vv = { 1, 2, 3, 4 };
-	vector<int> vvv = { 3, 2, 1 };
-	// -----
-
-	//while (next_permutation(vv.begin(), vv.end()))
-	do
-	{
-		for (auto it : vv)
-			cout << it << " ";
-		cout << "\n";
-	} while (next_permutation(vv.begin(), vv.end()));
-
-
-
-	//cout << prev_permutation(vv.begin(), vv.end()) << endl;
-	//cout << next_permutation(vv.begin(), vv.end()) << endl; // 1 3 2
-	//cout << next_permutation(vvv.begin(), vvv.end()) << endl;
-
-	//
-
-	return 0;
-	//std::reverse(v.begin(), v.end() - 2);
-	std::swap(v, vv);
-	int a = 1, b = 4;
-	a ^= b ^= a ^= b;
-
-
-
-	while (cin >> a)
-	{
-		pq.push(-a);
-	}
-
-	//make_heap
-	while (pq.size() > 0)
-	{
-		cout << pq.top() << " ";
-		pq.pop();
-	}
-
-
-
-	/*int n;
-	cin >> n;
-	vector<int> v;
-	for (int i = 0; i < n; ++i)
-	{
-		int a;
-		cin >> a;
-		v.push_back(a);
-	}
-
-
-	map<vector<int>, int> ma;
-
-	int k; cin >> k;
-
-
-	for (int i = 0; i < n - k; ++i)
-	{
-		vector<int> temp;
-		for (int j = 0; j < k; ++j)
-		{
-			temp.push_back(v[i+j]);
-		}
-		ma[temp]++;
-	}
-
-	for (auto it : ma)
-	{
-		vector<int> temp = it.first;
-
-		cout << "{";
-		for (auto et : temp)
-			cout << et;
-		cout << "} = ";
-		cout << it.second;
-		cout << "\n";
-	}*/
-
-
-
-
-	////vector<int> v = { 1, 4, 2, 3, 1, 3, 2, 32, 4, 3, 2, 4, 5, 3, 2, 1, 2, 1, 12, 234, 0, 39 , 34 };
-
-	////sort(v.begin(), v.end(), my_less);
-
-
-	//set<int, cmp> s = {11, 4, 3, 2, 4, 5, 3, 2, 1, 2, 1, 10, 12, 234, 0, 39 , 34 };
-
-	//for (auto it : s)
-	//	cout << it << " ";
-
-
-	/*set< set<int> > sv = { {1, 2, 3}, {3, 3, 4, 34}, {2, 2, 3, 3, 2,2} };*/
-
-
-	//// Ключ - сортироваться
-	//map<int, string, cmp> m;
-
-
-	//for (int i = 1; i < 30; ++i)
-	//{
-	//	int r = rand_gen();
-	//	m[i] = char('a');
-	//}
-
-	///*for (auto it : m)
-	//{
-	//	cout << it.first << " " << it.second << "\n";
-	//}*/
-
-	//m[11] = 'R';
-
-	//for (auto [l,r] : m)
-	//{
-	//	cout << l << " " << r << "\n";
-	//}
 
 }
